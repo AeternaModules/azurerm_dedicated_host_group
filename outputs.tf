64fbe8f66463f3a8e49e3f8cccb3cf69fd3e9ebd
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:47:45 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,33 @@
 output "dedicated_host_groups_id" {
   description = "Map of id values across all dedicated_host_groups, keyed the same as var.dedicated_host_groups"
-  value       = { for k, v in azurerm_dedicated_host_group.dedicated_host_groups : k => v.id }
+  value       = { for k, v in azurerm_dedicated_host_group.dedicated_host_groups : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "dedicated_host_groups_automatic_placement_enabled" {
   description = "Map of automatic_placement_enabled values across all dedicated_host_groups, keyed the same as var.dedicated_host_groups"
-  value       = { for k, v in azurerm_dedicated_host_group.dedicated_host_groups : k => v.automatic_placement_enabled }
+  value       = { for k, v in azurerm_dedicated_host_group.dedicated_host_groups : k => v.automatic_placement_enabled if v.automatic_placement_enabled != null }
 }
 output "dedicated_host_groups_location" {
   description = "Map of location values across all dedicated_host_groups, keyed the same as var.dedicated_host_groups"
-  value       = { for k, v in azurerm_dedicated_host_group.dedicated_host_groups : k => v.location }
+  value       = { for k, v in azurerm_dedicated_host_group.dedicated_host_groups : k => v.location if v.location != null && length(v.location) > 0 }
 }
 output "dedicated_host_groups_name" {
   description = "Map of name values across all dedicated_host_groups, keyed the same as var.dedicated_host_groups"
-  value       = { for k, v in azurerm_dedicated_host_group.dedicated_host_groups : k => v.name }
+  value       = { for k, v in azurerm_dedicated_host_group.dedicated_host_groups : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "dedicated_host_groups_platform_fault_domain_count" {
   description = "Map of platform_fault_domain_count values across all dedicated_host_groups, keyed the same as var.dedicated_host_groups"
-  value       = { for k, v in azurerm_dedicated_host_group.dedicated_host_groups : k => v.platform_fault_domain_count }
+  value       = { for k, v in azurerm_dedicated_host_group.dedicated_host_groups : k => v.platform_fault_domain_count if v.platform_fault_domain_count != null }
 }
 output "dedicated_host_groups_resource_group_name" {
   description = "Map of resource_group_name values across all dedicated_host_groups, keyed the same as var.dedicated_host_groups"
-  value       = { for k, v in azurerm_dedicated_host_group.dedicated_host_groups : k => v.resource_group_name }
+  value       = { for k, v in azurerm_dedicated_host_group.dedicated_host_groups : k => v.resource_group_name if v.resource_group_name != null && length(v.resource_group_name) > 0 }
 }
 output "dedicated_host_groups_tags" {
   description = "Map of tags values across all dedicated_host_groups, keyed the same as var.dedicated_host_groups"
-  value       = { for k, v in azurerm_dedicated_host_group.dedicated_host_groups : k => v.tags }
+  value       = { for k, v in azurerm_dedicated_host_group.dedicated_host_groups : k => v.tags if v.tags != null && length(v.tags) > 0 }
 }
 output "dedicated_host_groups_zone" {
   description = "Map of zone values across all dedicated_host_groups, keyed the same as var.dedicated_host_groups"
-  value       = { for k, v in azurerm_dedicated_host_group.dedicated_host_groups : k => v.zone }
+  value       = { for k, v in azurerm_dedicated_host_group.dedicated_host_groups : k => v.zone if v.zone != null && length(v.zone) > 0 }
 }
 
